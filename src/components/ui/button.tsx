9c685f6ae43cc7f3c101f7b0da807e828f9c126b
harshalmore31 <harshalmore2468@gmail.com>
2026-08-13 15:04:06 +0530

    style: prettier format the extracted button module

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -6,7 +6,6 @@ import { type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button-variants"
 
-
 function Button({
   className,
   variant = "default",
